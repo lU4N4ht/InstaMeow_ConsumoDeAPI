6994d94bd095ea03e982768f401dd94106a3422a
lU4N4ht <luana.maga78@gmail.com>
2023-10-18 13:43:08 -0300

    problema comn a api

--- photos_page.js
+++ photos_page.js
@@ -1,18 +1,26 @@
 'use strict'
+
+
+
+
 const decoration = [
     {
         imagem: 'cardDecoration.png',
-        texto: 'aaaaaaaaaa'
+        texto: 'Bengals are a lot of fun to live with, but theyre definitely not the cat for everyone, or for'
     }
 ]
-async function pegarImagens(){
+async function pegarImagens() {
     const url = 'https://api.thecatapi.com/v1/images/search'
     const response = await fetch(url)
     const imagens = await response.json()
+     console.log(imagens)
 
     return imagens.message
+
+   
 }
-function mostrarCards (decoration, imagem){
+function mostrarCards(decoration, imagem) {
+
     const cardContainer = document.getElementById('card_container')
     const cardDecoration = document.createElement('div')
     cardDecoration.classList.add('card-decoration')
@@ -24,22 +32,36 @@ function mostrarCards (decoration, imagem){
     const tagImg = document.createElement('img')
     tagImg.src = imagem
     const cardDescription = document.createElement('div')
-    cardDescription.classList.add('card-description')
+    cardDescription.classList.add('card_content')
     const text = document.createElement('h4')
     text.textContent = decoration.texto
     const cardReaction = document.createElement('div')
     cardReaction.classList.add('card_reaction')
+    const cardLink = document.createElement('a')
+    cardLink.href = './info_page.html'
+    const cardButton = document.createElement('button')
+    cardButton.textContent = 'See More!'
+    const buttonImg = document.createElement('img')
+    buttonImg.src = './img/seta-direita 1.png'
+    buttonImg.style.width = '30px'
 
 
-
-
-
-
-
-    const cargo = document.createElement('p')
-    cargo.textContent = funcionario.cargo
-
-    cardDescription.replaceChildren(nome, cargo)
-    card.replaceChildren(imagem, cardDescription)
-    cardContainer.appendChild(card)
+    cardButton.appendChild(buttonImg)
+    cardLink.appendChild(cardButton)
+    cardReaction.appendChild(cardLink)
+    cardDescription.appendChild(text)
+    cardDescription.appendChild(cardReaction)
+    card.appendChild(tagImg)
+    card.appendChild(cardDescription)
+    cardDecoration.appendChild(decorationImg)
+    cardDecoration.appendChild(card)
+    cardContainer.appendChild(cardDecoration)
 }
+async function carregarGaleria() {
+    const imagens = await pegarImagens()
+    imagens.forEach(mostrarCards)
+    decoration.forEach(mostrarCards)
+
+}
+carregarGaleria()
+
