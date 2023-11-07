18909aad5d509ebf2ac5ee55a7d34607a10aa39e
lU4N4ht <luana.maga78@gmail.com>
2023-11-07 20:40:14 -0300

    refazendo tudo do zero

--- picture.js
+++ picture.js
@@ -2,17 +2,15 @@
 
 const decoration = [
     {
-        imagem: 'cardDecoration.png',
-        texto: 'Bengals are a lot of fun to live with, but theyre definitely not the cat for everyone, or for'
+        imagem: 'cardDecoration.png'
     }
 ]
 
 async function pegarImagens() {
-    const url = 'https://api.thecatapi.com/v1/images/search?limit=20'
+    const url = 'https://api.thecatapi.com/v1/images/search?limit=10'
     const response = await fetch(url)
     const imagens = await response.json()
     return imagens
-   
 }
     
 function mostrarCards(imagem) {
@@ -31,28 +29,8 @@ function mostrarCards(imagem) {
         const tagImg = document.createElement('div')
         tagImg.style.backgroundImage = `url(${imagem.url})`
         tagImg.classList.add('imagem-animal')
-        const cardDescription = document.createElement('div')
-        cardDescription.classList.add('card_content')
-        const text = document.createElement('h4')
-        text.textContent = decoration.texto
-        const cardReaction = document.createElement('div')
-        cardReaction.classList.add('card_reaction')
-        const cardLink = document.createElement('a')
-        cardLink.href = './info_page.html'
-        const cardButton = document.createElement('button')
-        cardButton.textContent = 'See More!'
-        const buttonImg = document.createElement('img')
-        buttonImg.src = './img/seta-direita 1.png'
-        buttonImg.style.width = '30px'
     
-    
-        cardButton.appendChild(buttonImg)
-        cardLink.appendChild(cardButton)
-        cardReaction.appendChild(cardLink)
-        cardDescription.appendChild(text)
-        cardDescription.appendChild(cardReaction)
         card.appendChild(tagImg)
-        card.appendChild(cardDescription)
         cardDecoration.appendChild(decorationImg)
         cardDecoration.appendChild(card)
         cardContainer.appendChild(cardDecoration)
@@ -66,6 +44,7 @@ async function carregarGaleria() {
     mostrarCards(imagens)
 
 }
+
 
 carregarGaleria()
 
